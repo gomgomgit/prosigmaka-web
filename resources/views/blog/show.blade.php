e1c14417bef2aad37d727b432f4a5eca500cdd36
gomgomgit <thechocohead@gmail.com>
2026-07-31 16:25:39 +0700

    Revert "SEO google"
    This reverts commit 9d3e8b356c3ab987c311472c244ffaf660140d65.

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,13 +1,4 @@
-<x-layouts.landing
-    :canonical="$seo['canonical']"
-    :description="$seo['description']"
-    :image="$seo['image']"
-    :modified-time="$seo['modifiedTime']"
-    :published-time="$seo['publishedTime']"
-    :structured-data="$seo['structuredData']"
-    :title="$seo['title']"
-    :type="$seo['type']"
->
+<x-layouts.landing :title="$post->title.' - ProSigmaka'">
     <x-site-navigation :sections="$sections" />
 
     <main id="main-content" class="blog-page">
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\LandingPageController;
-use App\Http\Controllers\SeoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', LandingPageController::class)->name('landing');
-Route::get('/robots.txt', [SeoController::class, 'robots'])->name('robots');
-Route::get('/sitemap.xml', [SeoController::class, 'sitemap'])->name('sitemap');
 Route::get('/blog/{post:slug}', [BlogController::class, 'show'])->name('blog.show');
